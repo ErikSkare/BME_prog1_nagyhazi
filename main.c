ed923245b2a89f4fdfb0957654cd88ea6c70ff17
ErikSkare <skareerik55@gmail.com>
2021-11-13 16:27:54 +0100

    main

--- main.c
+++ main.c
@@ -1,27 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "funkciok.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+    if(argc != 2) {
+        fprintf(stderr, "A program megfelelo hivasa: etterem.exe [etterem neve]!\n");
+        return 1;
+    }
+
+    //Fájlnevek létrehozása
+    char asztalok_fajlnev[255], menu_fajlnev[255], rendelesek_fajlnev[255];
+    strcpy(asztalok_fajlnev, argv[1]);
+    strcat(asztalok_fajlnev, "_asztalok.csv");
+    strcpy(menu_fajlnev, argv[1]);
+    strcat(menu_fajlnev, "_menu.csv");
+    strcpy(rendelesek_fajlnev, argv[1]);
+    strcat(rendelesek_fajlnev, "_rendelesek.csv");
+
+    //Adatok beolvasása és hibakezelés
     Asztalok asztalok;
     Menu menu;
 
-    menu_beolvas("menu.csv", &menu);
-    asztalok_beolvas("asztalok.csv", &asztalok);
-    rendelesek_beolvas("rendelesek.csv", &menu, &asztalok);
+    bool asztal_hozzaad_hiba = asztalok_beolvas(asztalok_fajlnev, &asztalok) == 2;
+    bool menupont_hozzaad_hiba = menu_beolvas(menu_fajlnev, &menu) == 2;
+    bool rendeles_hozzaad_hiba = rendelesek_beolvas(rendelesek_fajlnev, &menu, &asztalok) == 2;
 
-    Rendeles *aktualis = asztal_keres(1, &asztalok)->hely_rendelesek[0].eleje;
-    while(aktualis != NULL) {
-        printf("%s %d\n", aktualis->termek->nev, aktualis->darab);
-        aktualis = aktualis->kov;
+    if(asztal_hozzaad_hiba || menupont_hozzaad_hiba || rendeles_hozzaad_hiba) {
+        fprintf(stderr, "Nem sikerult a listak letrehozasa!\n");
+        asztalok_felszabadit(&asztalok);
+        menu_felszabadit(&menu);
+        return 2;
     }
 
-    rendelesek_kiir("rendelesek.csv", &asztalok);
-    menu_kiir("menu.csv", &menu);
-    asztalok_kiir("asztalok.csv", &asztalok);
+    //Konzolos menüvel vezérlés (még készülõben)
 
-    menu_felszabadit(&menu);
+    //Adatok kiírása
+    asztalok_kiir(asztalok_fajlnev, &asztalok);
+    menu_kiir(menu_fajlnev, &menu);
+    rendelesek_kiir(rendelesek_fajlnev, &asztalok);
+
+    //Felszabadítás
     asztalok_felszabadit(&asztalok);
+    menu_felszabadit(&menu);
     return 0;
 }
